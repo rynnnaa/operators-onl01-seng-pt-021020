c46e6ad95d000f30bf5107697c8f6d9ae8860544
Ryna Truong <rynatruong@outlook.com>
2020-02-13 04:00:26 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -6,9 +6,9 @@ def unsafe?(speed)
   end
 end
 
-# def not_safe?(speed)
-# 	speed < 40 || speed > 60 : speed > 40 || speed < 60
-# end
+def not_safe?(speed)
+	speed < 40 || speed > 60 : speed > 40 || speed < 60
+end
 	
 
 
